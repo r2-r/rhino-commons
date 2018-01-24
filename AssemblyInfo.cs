5c84285d152dd7cb69719c082d91ff6a5367124b
r2-r <artur.grzegorzewicz@gmail.com>
2018-01-24 19:07:01 +0100

    Make VS 2017 compatible
    * Remove psake dependency,
    * Update VS solution,
    * Remove signing.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rhino.Commons for Microsoft .NET Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rhino.Commons")]
+[assembly: AssemblyCopyright("Rhino Tools Project, original author or authors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("287f5d01-15a4-4d17-938c-05f4087edefd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
--- psake.ps1
+++
@@ -1,5 +0,0 @@
-# Helper script for those who want to run
-# psake without importing the module.
-import-module .\psake.psm1
-invoke-psake @args  -framework 4.0
-remove-module psake
